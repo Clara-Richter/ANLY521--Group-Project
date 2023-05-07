af61d5acbb268799f19bf77c925ada059bd98c7a
Ryan Walter <rcw76@georgetownn.edu>
2023-05-07 14:21:58 -0400

    main.py

--- Code/bin/main.py
+++ Code/bin/main.py
@@ -6,37 +6,35 @@ import spacy
 from Code.utils.load import load_text
 from Code.utils.display import display_entities
 from Code.entities import extract_entities
-# from Code.summarization import SumText
+from Code.summarization import SumText
+import webbrowser
 
-def main(input_dir):
-    # Load text data
-    text = load_text(input_dir)
-    ent_txt = extract_entities(text)
-    # display
-    display_entities(text)
-
-    # sum_list = []
-    # def_list = []
-    # for row in df['transcription']:
-    #     res = SumText(row)
-    #     res = res.summarize()
-    #     res_def = SumText.add_definitions(res)
-    #     sum_list.append(res)
-    #     def_list.append(res_def)
-    # df['summary'] = sum_list
-    # df['summary_definitions'] = def_list
-
-
-        
-if __name__ == '__main__':
-    # Set up argparse
+def main():
     parser = argparse.ArgumentParser(description='understand medical documents.')
     parser.add_argument("-f", "--indir", required=True, help='Path to input file')
-    #### add add_argument(s) to specify summarization, definitions, entities, NER
+    parser.add_argument("-t", "--type", required=True,
+                        choices=["summary", "definitions", "entities", "display"],
+                        help='Choose type of simplifier')
     args = parser.parse_args()
+    text = load_text(args.indir)
+    if args.type == 'summary':
+        result = SumText(text)
+        result = result.summarize()
+        print(result)
+    elif args.type == 'definitions':
+        result = SumText(text)
+        result = result.add_definitions()
+        print(result)
+    elif args.type == 'entities':
+        result = extract_entities(text)
+        print(result)
+    elif args.type == 'display':
+        display_entities(text)
 
-    # Call main function with input directory as argument
-    main(args.indir)
+
+if __name__ == '__main__':
+    main()
+
 
 # call like this: python3 Code/bin/main.py --indir Data/test_text.txt
 
